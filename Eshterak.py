ae595f8e33207256f9cec91a3c10f4237b67b67c
Ali Askari <90070062+ConnellOutis@users.noreply.github.com>
2022-06-10 02:58:16 +0430

    Add files via upload

---
+++ Eshterak.py
@@ -0,0 +1,17 @@
+import os 
+
+os.system('cls') 
+
+list1 = [1,2,3,4,5] 
+list2 = [2,5,4,7,8] 
+
+set1 = set(list1) 
+set2 = set(list2) 
+
+inter = set1.intersection(set2)
+
+print(set1) 
+
+print(set2) 
+
+print(inter)
